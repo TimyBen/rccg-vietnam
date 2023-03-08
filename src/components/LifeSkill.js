b5622af49a96a3c96822140070c5eee81f3bd78d
Timothy <timothy@ahamove.com>
2023-03-08 15:00:29 +0700

    responsiveness fix

--- src/components/LifeSkill.js
+++ src/components/LifeSkill.js
@@ -7,21 +7,21 @@ function LifeSkill() {
     <div className="">
       <div className="w-full h-full">
         <div className="w-full bg-gradient-to-r from-[#002751] to-[#036871] ">
-          <header className="w-full h-fit flex items-center justify-between">
+          <header className="w-full h-fit flex max-[420px]:flex-col items-center justify-between">
             <Link
               to={"/"}
               className="px-6 py-6 flex justify-center items-center h-fit"
             >
-              <img className="w-28 h-28" src={logo} alt="" />
+              <img className="w-28 h-28 max-[420px]:w-12 max-[420px]:h-12" src={logo} alt="" />
               <div className="text-xl justify-center pl-6 items-center  font-bold text-white leading-loose">
-              <h1 className="text-3xl tracking-wider font-climate leading-normal font-Bold">
-                Harvest Center Parish
-              </h1>
-              <h1 className="text-2xl tracking-wider font-serif font-Bold">Vietnam</h1>
-            </div>
+                <h1 className="text-3xl tracking-wider font-climate leading-normal font-Bold max-[420px]:text-base">
+                  Harvest Center Parish
+                </h1>
+                <h1 className="text-2xl tracking-wider font-serif font-Bold max-[420px]:text-xs">Vietnam</h1>
+              </div>
             </Link>
-            <nav className="pr-20 h-fit w-[30%] navbar navbar-expand-lg navbar-light">
-              <ul className="w-full text-white justify-between content-center leading-normal font-serif font-medium text-xl pl-2 pr-12 py-12 flex navbar-nav mr-auto">
+            <nav className="lg:px-32 h-fit max-[420px]:w-full navbar navbar-expand-lg navbar-light">
+              <ul className="max-[420px]:text-base w-full text-white justify-between content-center leading-normal font-serif font-medium text-xl max-[420px]:pr-4  max-[420px]:py-12 flex navbar-nav mr-auto">
                 <li className="p-3">
                   <button className="ease-linear transform hover:scale-125 transition duration-300">
                     <Link to={"/"} className="nav-link">
@@ -55,10 +55,10 @@ function LifeSkill() {
           </header>
         </div>
         <div className="h-fit w-full">
-          <div className="w-full h-[30rem] absolute z-10 ">
-            <div className="w-full h-[30rem] flex justify-center items-center">
-              <div className="flex justify-center content-center h-[5rem] w-[60%] bg-[#154163] rounded-t-3xl rounded-b-3xl absolute bottom-0">
-                <h1 className="justify-center flex self-center text-4xl font-serif text-white tracking-wide font-Bold">
+          <div className="w-full lg:h-[30rem] max-sm:h-[27vh] max-lg:h-[40.5vh] absolute z-10 ">
+            <div className="w-full lg:h-[30rem] flex justify-center items-center">
+              <div className="flex justify-center content-center lg:h-[5rem] max-sm:h-12 max-lg:h-16 w-[60%] bg-[#154163] bg-opacity-90 rounded-tl-3xl rounded-br-3xl absolute bottom-0">
+                <h1 className="justify-center flex self-center text-4xl max-sm:text-2xl font-serif text-white tracking-wide font-Bold">
                   Life Skill Training
                 </h1>
               </div>
@@ -66,7 +66,7 @@ function LifeSkill() {
           </div>
           <div className="w-full h-fit">
             <img
-              className="w-full h-[30rem] aspect-auto bg-center bg-contain bg-no-repeat"
+              className="w-full lg:h-[30rem] object-contain aspect-auto bg-center bg-contain bg-no-repeat"
               src={LifeSkillLogo}
               alt=""
             />
--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import logo from "../assets/rccg_logo.png";
 import { Link } from "react-router-dom";
-import Img_1 from "../assets/Img_1.jpg";
+import Img_1 from "../assets/Img_11.JPG";
 import Img_2 from "../assets/Img_2.jpg";
 // import Img_3 from "../assets/Img_1.jpg";
 import { Fade } from "react-slideshow-image";
@@ -12,23 +12,23 @@ import Brief from "./brief";
 function Home() {
   return (
     <div className="h-full">
-      <div className="w-full h-full">
-        <div className="h-full w-full bg-gradient-to-r from-[#002751] to-[#036871] ">
-          <header className="w-full h-fit flex items-center justify-between">
+      <div className="w-full h-full mx-auto">
+        <div className="h-full w-full bg-gradient-to-r from-[#002751] to-[#036871] flex justify-between items-center">
+          <header className="w-full h-fit flex max-[420px]:flex-col items-center justify-between">
             <Link
               to={"/"}
               className="px-6 py-6 flex justify-center items-center h-fit"
             >
-              <img className="w-28 h-28" src={logo} alt="" />
+              <img className="w-28 h-28 max-[420px]:w-12 max-[420px]:h-12" src={logo} alt="" />
               <div className="text-xl justify-center pl-6 items-center  font-bold text-white leading-loose">
-                <h1 className="text-3xl tracking-wider font-climate leading-normal font-Bold">
+                <h1 className="text-3xl tracking-wider font-climate leading-normal font-Bold max-[420px]:text-base">
                   Harvest Center Parish
                 </h1>
-                <h1 className="text-2xl tracking-wider font-serif font-Bold">Vietnam</h1>
+                <h1 className="text-2xl tracking-wider font-serif font-Bold max-[420px]:text-xs">Vietnam</h1>
               </div>
             </Link>
-            <nav className="pr-20 h-fit w-[30%] navbar navbar-expand-lg navbar-light">
-              <ul className="w-full text-white justify-between content-centerleading-normal font-serif font-medium text-xl pl-2 pr-12 py-12 flex navbar-nav mr-auto">
+            <nav className="lg:px-32 h-fit max-[420px]:w-full navbar navbar-expand-lg navbar-light">
+              <ul className="max-[420px]:text-base w-full text-white justify-between content-center leading-normal font-serif font-medium text-xl max-[420px]:pr-4  max-[420px]:py-12 flex navbar-nav mr-auto">
                 <li className="p-3">
                   <button className="ease-linear transform hover:scale-125 transition duration-300">
                     <Link to={"/"} className="nav-link">
@@ -67,14 +67,14 @@ function Home() {
             <Fade className="w-full">
               <div className="each-fade">
                 <img
-                  className="w-full h-fit aspect-auto bg-center bg-contain bg-no-repeat blur-[2px]"
+                  className="w-full h-fit aspect-auto object-contain bg-center bg-contain bg-no-repeat blur-[2px]"
                   src={Img_1}
                   alt=""
                 />
                 <div className="absolute flex justify-center items-center  top-0 w-full h-full">
 
-                  <div className="h-full  font-serif font-medium w-full py-32 px-32 bg-[#1d30411a] text-[#ffffff] flex justify-center items-center ">
-                    <h1 className="w-[60rem] animate-fade h-[35rem] text-[4rem] text-center rounded-lg uppercase leading-loose antialiased bg-[#0104063c] tracking-wide description flex justify-center items-center">
+                  <div className="h-full max-[420px]:w-full font-serif font-medium w-full lg:py-32 lg:px-32 bg-[#1d30411a] text-[#ffffff] flex justify-center items-center ">
+                    <h1 className="w-[60rem] animate-fade h-[35rem] text-[3rem] max-[420px]:text-base text-center rounded-lg uppercase leading-loose antialiased bg-[#0104063c] tracking-wide description flex justify-center items-center">
                       Live by Faith <br /> be known for Love <br /> & be a voice of Hope.
                     </h1>
                     {/* <div className="w-full h-full blur-[10px] bg-black absolute"></div> */}
@@ -83,13 +83,13 @@ function Home() {
               </div>
               <div className="each-fade">
                 <img
-                  className="w-full h-[93vh] aspect-auto bg-center bg-contain bg-no-repeat brightness-5"
+                  className="w-full h-fit object-fill aspect-auto bg-center bg-contain bg-no-repeat brightness-5"
                   src={Img_2}
                   alt=""
                 />
                 <div className="absolute flex justify-center items-center  top-0 w-full h-full">
-                  <div className="h-full  font-serif font-medium w-full py-32 px-32 bg-[#24354512] text-[#f4faff] flex justify-center items-center ">
-                    <h1 className="w-[60rem] animate-fade h-[35rem] text-[4rem] text-center rounded-lg uppercase leading-loose antialiased bg-[#01040622] tracking-wide description flex justify-center items-center">
+                  <div className="h-full  font-serif font-medium w-full lg:py-32 lg:px-32 bg-[#24354512] text-[#f4faff] flex justify-center items-center ">
+                    <h1 className="w-[60rem] animate-fade h-[35rem] text-[3rem] max-[420px]:text-base text-center rounded-lg uppercase leading-loose antialiased bg-[#01040622] tracking-wide description flex justify-center items-center">
                       Harvest Center Parish <br /> Football team
                     </h1>
                   </div>
@@ -99,7 +99,7 @@ function Home() {
           </div>
           <div className="h-full w-full flex flex-col justify-center items-center">
             <div className="w-full h-32 flex bg-[#0e485267] bg-opacity-30 justify-center items-center pt-40 pb-10">
-              <h1 className="description text-4xl drop-shadow-2xl uppercase leading-loose tracking-widest animate-bounce text-black text-[2rem] font-serif font-light text-center">how can we assist <br /> you today ?</h1>
+              <h1 className="description text-4xl drop-shadow-2xl uppercase leading-loose tracking-widest animate-bounce text-black text-[2rem] font-serif font-light text-center max-[420px]:text-base">how can we assist <br /> you today ?</h1>
             </div>
             <div className="w-full h-full">
               <Cont />
